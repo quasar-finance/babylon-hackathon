a2f5041675981d0f37a1b879535100928b341eb2
0xLaurens <32776056+0xLaurenzo@users.noreply.github.com>
2024-11-05 15:26:03 +0700

    Add mock gauge (#5)
    * move interfaces to packages
    
    * Add mock gauge

---
+++ contracts/mock-gauge/src/tests/setup.rs
@@ -0,0 +1,58 @@
+use crate::{
+    contract::{instantiate, reply},
+    msg::InstantiateMsg,
+    state::Destination,
+};
+use cosmwasm_std::{
+    from_json,
+    testing::{
+        mock_dependencies, mock_dependencies_with_balances, mock_env, mock_info, MockApi,
+        MockQuerier, MockStorage,
+    },
+    to_json_binary, Coin, ContractResult, Decimal, Empty, OwnedDeps, QuerierResult, Reply,
+    SubMsgResponse, SubMsgResult, SystemError, SystemResult, WasmQuery,
+};
+use prost::Message;
+use quasar_std::quasarlabs::quasarnode::tokenfactory::v1beta1::MsgCreateDenomResponse;
+
+pub const OWNER: &str = "owner";
+pub const USER: &str = "user";
+pub const SUBDENOM: &str = "subdenom";
+pub const DEPOSIT_DENOM: &str = "denom1";
+pub const OTHER_DEPOSIT_DENOM: &str = "denom2";
+pub const VAULT_DENOM: &str = "vault_denom";
+
+pub const DESTINATION_IDS: [&str; 2] = ["id1", "id2"];
+
+fn basic_setup(
+    deps: OwnedDeps<MockStorage, MockApi, MockQuerier, Empty>,
+) -> OwnedDeps<MockStorage, MockApi, MockQuerier, Empty> {
+    let mut deps = deps;
+    let env = mock_env();
+    let info = mock_info(USER, &[]);
+
+    assert!(instantiate(
+        deps.as_mut(),
+        env.clone(),
+        info,
+        InstantiateMsg {
+            owner: OWNER.to_string(),
+            destinations: DESTINATION_IDS.iter().map(|&s| s.to_string()).collect(), // this is really ugly and should be done simpler
+        },
+    )
+    .is_ok());
+
+    deps
+}
+
+pub fn setup() -> OwnedDeps<MockStorage, MockApi, MockQuerier, Empty> {
+    let deps = mock_dependencies();
+    basic_setup(deps)
+}
+
+pub fn setup_with_balances(
+    balances: &[(&str, &[Coin])],
+) -> OwnedDeps<MockStorage, MockApi, MockQuerier, Empty> {
+    let deps = mock_dependencies_with_balances(balances);
+    basic_setup(deps)
+}
---
+++ packages/interfaces/src/gauge.rs
@@ -0,0 +1,42 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Decimal, Empty, Uint128};
+
+#[cw_serde]
+pub enum ExecuteMsg<T = Empty> {
+    AddDestination { destination_id: String },
+    Custom(T),
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg<T = Empty> {
+    #[returns(GetAllocationsResponse)]
+    GetAllocations {},
+    #[returns(GetAllocationResponse)]
+    GetAllocation { denom: String },
+    #[returns(GetDestinationsResponse)]
+    GetDestinations {},
+    #[returns(Empty)]
+    Custom(T),
+}
+
+#[cw_serde]
+pub struct GetAllocationsResponse {
+    pub allocations: Vec<Allocation>,
+}
+
+#[cw_serde]
+pub struct GetAllocationResponse {
+    pub allocation: Allocation,
+}
+
+#[cw_serde]
+pub struct Allocation {
+    pub destination_id: String,
+    pub amount: Decimal,
+}
+
+#[cw_serde]
+pub struct GetDestinationsResponse {
+    pub destinations: Vec<String>,
+}
