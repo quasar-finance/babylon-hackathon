c22d477b914678bc4170cb59845064efcaaf84bd
Laurens <32776056+0xLaurenzo@users.noreply.github.com>
2024-11-06 15:13:59 +0700

    add instantiation

--- scripts/smart-contracts/instantiate.sh
+++ scripts/smart-contracts/instantiate.sh
@@ -11,3 +11,45 @@ done
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 source ${REPO_ROOT}/scripts/smart-contracts/env_euphrates.sh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+INPUT=$1
+
+get_code_id() {
+    local filename=$1
+    local contract_name=${filename%.wasm}  # Remove .wasm extension
+    local code_id=$(jq -r ".[\"$contract_name\"]" "${REPO_ROOT}/scripts/smart-contracts/code_ids.json")
+    
+    if [ "$code_id" == "null" ]; then
+        echo "Error: Contract $contract_name not found in code_ids.json"
+        exit 1
+    fi
+    echo $code_id
+}
+
+
+CODE_ID=$(get_code_id "$INPUT")
+echo "Using Code ID: $CODE_ID"
+
+
+res=$(babylond tx wasm instantiate $CODE_ID '{"owner":"bbn1knv468atwzjk4v0d22jwa497v0sd0zez3lh7g3","destinations":["bbn1knv468atwzjk4v0d22jwa497v0sd0zez3lh7g3"]}' $keyringBackend --from=$userKey --admin=$address --label="test-label" --gas=auto --gas-prices 0.01u$feeToken --gas-adjustment=1.3 --chain-id=$chainId -b=sync -y --log_format=json -o "json" --node $nodeUrl)
+txhash=$(echo "$res" | jq -r '.txhash')
+echo "Transaction hash: $txhash"
+sleep 45
+address=$(babylond q tx $txhash -o json --node $nodeUrl | jq -r '.events[] | select(.type == "instantiate").attributes[] | select(.key == "_contract_address").value')
+echo "Contract address: $address"
+
+json_file="${REPO_ROOT}/scripts/smart-contracts/contract_addresses.json"
+
+if [ ! -f "$json_file" ]; then
+    echo "{}" > "$json_file"
+fi
+
+# Set the code id in the json file
+filename=$(basename "$INPUT" .wasm)
+jq --arg name "$filename" --arg id "$address" \
+    '. + {($name): $id}' "$json_file" > "$json_file.tmp" && mv "$json_file.tmp" "$json_file"
