cfc64f428fee7ffccfff746a472fd219c7aa4888
Arham Chordia <43543921+arhamchordia@users.noreply.github.com>
2024-11-07 14:15:53 +0700

    Updated scripts and deployment addresses. (#25)
    * Adding deployed contracts
    
    * Adding deployment scripts for vault, oracle and adaptor.
    
    * Correcting typo
    
    * Updated addresses and deployement scripts.

--- scripts/smart-contracts/babylon_vault.sh
+++ scripts/smart-contracts/babylon_vault.sh
@@ -19,3 +19,8 @@ MOCK_GAUGE_ADDRESS=$(jq -r '.mock_gauge' "${REPO_ROOT}/scripts/smart-contracts/c
 
 bash $CONTRACT_SCRIPTS/store.sh babylon_vault.wasm
 bash $CONTRACT_SCRIPTS/instantiate.sh babylon_vault '{"owner":"bbn1knv468atwzjk4v0d22jwa497v0sd0zez3lh7g3", "subdenom":"quasar-awesome-babylon-vault-token-for-shared-security", "oracle" :"'$DUMMY_ORACLE_ADDRESS'", "gauge": "'$MOCK_GAUGE_ADDRESS'"}'
+
+sleep 10
+
+BABYLON_VAULT_ADDRESS=$(jq -r '.babylon_vault' "${REPO_ROOT}/scripts/smart-contracts/contract_addresses.json")
+babylond tx wasm execute $BABYLON_VAULT_ADDRESS '{"register_lst":{"denom":"ubbn"}}' $keyringBackend --from=$userKey --gas=auto --gas-prices 0.01u$feeToken --gas-adjustment=1.3 --chain-id=$chainId -b=sync -y --log_format=json -o "json" --node $nodeUrl
---
+++ scripts/smart-contracts/final_setup.sh
@@ -0,0 +1,15 @@
+sh mock_gauge.sh
+
+sleep 45
+
+sh dummy_oracle.sh
+
+sleep 45
+
+sh babylon_vault.sh
+
+sleep 45
+
+sh ecosystem_adaptor.sh
+
+sleep 45
