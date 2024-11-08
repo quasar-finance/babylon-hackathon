a2e146418676d3a54f3fa1040859d77ec1e497a6
Arham Chordia <chordia.1@iitj.ac.in>
2024-11-08 11:24:30 +0700

    Updating gauge description.

--- site/src/lib/components/voting/VotingIncentives.svelte
+++ site/src/lib/components/voting/VotingIncentives.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	export let text = `Every epoch the rebalance from all the new purchases will kick in from the overall pool based on the shares purchsed by the protocols.`;
-	export let title = `Shared security rebalance`;
+	export let text = `Liquidity is allocated two fold: 50% is allocated based on shared security spending to Babylon, the other 50% based on veBBN and Bribes. BBN holders allocate liquidity to their BSN and other Babylon Ecosystem destinations of their choice. 
+
+Projects can lobby and advocate for receiving liquidity, allowing new projects to bootstrap initial liquidity, and mature projects to direct liquidity strategically into growth. 
+
+BLT depositors benefit from a stronger Babylon ecosystem & higher yields. The vault rebalances it’s liquidity allocation once per week, subject to the latest vote outcome`;
+	export let title = `Shared security.`;
 </script>
 
 <div class="voting-incentives">
